8bf379a78e56dd44618a161fa878d34bbe6655f8
swetasri13 <shefali.sri13@gmail.com>
2023-03-16 08:26:40 +0530

    Update mongo_client.py
    update logic to insert for SP500 constituents data in mongo db

--- mongo_client.py
+++ mongo_client.py
@@ -16,22 +16,35 @@ from pymongo import MongoClient
 
 yfin.pdr_override()
 
-stockList=[]
-stockList= ['GOOGL']
+# insert S&P500 constituent data
+sp_500_df= pd.read_csv('C:\sweta\portfolio-recommendation-engine\data\SP500_Companies.csv')
+#stockList1=sp_500_df["Symbol"].astype(str).tolist()
+
+#print(stockList1)
+stockList= ['GOOGL','APPL','ACE', 'ABT', 'ANF', 'ACN', 'ADBE', 'AMD', 'AES', 'AET', 'AFL', 'A', 'GAS', 'APD', 'ARG', 'AKAM', 'AA', 'ALXN']
 
 enddate=dt.datetime.now()
 startdte=enddate-dt.timedelta(days=365)
 stockData = pdr.get_data_yahoo(stockList, startdte, enddate)
 
 client =  MongoClient("mongodb+srv://mongouser:market101@cluster0.fgigdzs.mongodb.net/?retryWrites=true&w=majority")
+#client=MongoClient('mongodb+srv://mongouser:market101@cluster0.fgigdzs.mongodb.net/journaldev?authSource=admin',
+ #   server_selection_timeout=120000)
+print("Connection done")
 db = client['journaldev']
 collection = db['stock_returns']
 stockData.reset_index(inplace=True)
 data_dict = stockData.to_dict("records")
-# Insert collection
+# Insert collection EOD prices
 collection.insert_many(data_dict)
 
-   
+#inset SP 500 constituents data
+sp500_collection = db["sp500_data"]
+sp_500_df.reset_index(inplace=True)
+sp_data_dict = sp_500_df.to_dict("records") 
+#print(sp_data_dict) 
+# Insert SP500 data collection which is list of dictionary object
+sp500_collection.insert_many(sp_data_dict)
 
 
 # In[ ]:
